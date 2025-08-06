2bd585b4ca7cc0457e758e38787e4e8304df4552
pippocrespi <pippocrespi@gmail.com>
2025-08-06 16:03:29 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,46 +1,25 @@
 const CACHE_NAME = 'scheda-offline-cache-v1';
 
 const urlsToCache = [
-  '/',
   '/APP-Scheda/',
   '/APP-Scheda/index.html',
-  '/APP-Scheda/style.css',
-  '/APP-Scheda/script.js',
-  'https://code.jquery.com/jquery-3.7.1.min.js', // esempio esterno
+  '/APP-Scheda/icon-192.png',
+  '/APP-Scheda/icon-512.png',
+  'https://code.jquery.com/jquery-3.7.1.min.js',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js',
-  // Aggiungi qui tutte le altre risorse che usi (font, icone, immagini...)
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
-      // Serve dalla cache, altrimenti prova dal network
       return response || fetch(event.request);
-    }).catch(() => {
-      // Eventuale fallback offline
-    })
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames =>
-      Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      )
-    )
+    }).catch(() => caches.match('/APP-Scheda/index.html'))
   );
 });
