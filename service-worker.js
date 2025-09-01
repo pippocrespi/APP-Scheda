bbea85c1242ac748647ddda0df9c0f4293c02f1f
pippocrespi <pippocrespi@gmail.com>
2025-09-01 15:42:04 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "v1"; // <--- cambia numero a ogni aggiornamento
+const CACHE_VERSION = "v12"; // <--- cambia numero a ogni aggiornamento
 const CACHE_NAME = `scheda-offline-cache-${CACHE_VERSION}`;
 
 const urlsToCache = [
