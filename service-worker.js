366e11c383cf156fa0af12a14a623deb06832f2a
pippocrespi <pippocrespi@gmail.com>
2025-09-13 18:59:46 +0200

    Modifiche grafiche

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "v1.2"; // <--- cambia numero a ogni aggiornamento
+const CACHE_VERSION = "v1.2.1"; // <--- cambia numero a ogni aggiornamento
 const CACHE_NAME = `scheda-offline-cache-${CACHE_VERSION}`;
 
 const urlsToCache = [
